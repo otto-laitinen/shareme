c6eb15074748d984248ffc03dfacce133681bdb8
Otto <otto.laitinen@edu.turkuamk.fi>
2022-01-28 10:49:55 +0200

    routing

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,11 +1,16 @@
 // type in rafce to get the basic functional component
 
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const App = () => {
-  return <div>
-      herro herro
-  </div>;
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+
+    </Routes>
+  )
 };
 
 export default App;
