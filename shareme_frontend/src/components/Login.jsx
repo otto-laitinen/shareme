162e1b2a030f8061b8de54a7b86daed248da1ae6
Otto <otto.laitinen@edu.turkuamk.fi>
2022-01-28 12:17:26 +0200

    login and home page

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { userNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icon/fc'; // Google logo
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
